78ed72e133016cbeacd21d93d76c0bac60fb7721
Yakup <yakupjoraev@gmail.com>
2023-09-26 22:52:37 +0300

    added new page

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,61 @@
 // Custom scripts
-var swiper = new Swiper(".hero-slider", {
-  slidesPerView: 1,
-  spaceBetween: 10,
-  pagination: {
-    el: ".hero-slider__pagination",
-  },
-  navigation: {
-    nextEl: ".hero-slider__arrow--next",
-    prevEl: ".hero-slider__arrow--prev",
-  },
+function treningAffSlider() {
+  const container = document.querySelector('.trening-aff-slider__container');
 
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10
-    },
-    // when window width is >= 480px
-    767: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 640px
-    992: {
-      slidesPerView: 3,
-      spaceBetween: 46,
-    }
+  if (!container) {
+    return null
   }
+  var swiper = new Swiper(".trening-aff-slider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".trening-aff-slider__pagination",
+    },
+    navigation: {
+      nextEl: ".trening-aff-slider__arrow--next",
+      prevEl: ".trening-aff-slider__arrow--prev",
+    },
+
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10
+      },
+      // when window width is >= 480px
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+      // when window width is >= 640px
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 25,
+      }
+    }
+  });
+}
+
+treningAffSlider();
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const membersItems = document.querySelectorAll("[data-more-text-container]");
+
+  membersItems.forEach(item => {
+    const toggleButton = item.querySelector("[data-toggle-more-button]");
+    const textBlock = item.querySelector("[data-banner-descr-more]");
+
+    item.addEventListener("click", function () {
+      this.classList.toggle("banner-descr-more");
+      textBlock.classList.toggle("banner-descr-more");
+
+      if (textBlock.classList.contains("banner-descr-more")) {
+        toggleButton.textContent = "Свернуть";
+      } else {
+        toggleButton.textContent = "Подробнее";
+      }
+    });
+
+  });
 });
